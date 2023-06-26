1fcd1ad4af47a4d365078138c8c26f7924544ec0
DeBasti <sebastian.eisenbarth96@gmail.com>
2023-06-26 15:29:13 +0200

    enabled 4th thumbkey (added matrix entriy at bottom 3,3 and 7,0

--- keyboards/pteron36/keymaps/bastrah/keymap.c
+++ keyboards/pteron36/keymaps/bastrah/keymap.c
@@ -110,16 +110,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * |        |   Z  |   X  |   C  |   D  |   V  |      |      |  |      |      |   K  |   H  |   ,  |   .  |   ?  |        |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |      |      | NAV/| NUM  |      |  |      | SYM  | SHFT |      |      |
- *                        |      |      | SPC | TAB  |      |  |      | ENT  | BSPC |      |      |
+ *                        |      |win   | NAV/ | NUM  |Teams |  |      | SYM  | SHFT |win   |      |
+ *                        |      |left  | SPC  | TAB  |Mute  |  |      | ENT  | BSPC |right |      |
  *                        `----------------------------------'  `----------------------------------'
+ * I manually added the innermost thumb key into info.json but cannot find the correct position
+ * therefore the keymap beneath does not match the real life but the keymap comment above does
  */
-    [_base] = LAYOUT_split_3x5_3_encoder(
+    [_base] = LAYOUT_split_3x5_4_encoder(
         KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                                                   KC_J,    KC_L,    KC_U,    KC_Z,    LSFT(KC_NONUS_HASH),
         HOME_A,  HOME_R,  HOME_S,  HOME_T,  KC_G,                                                   KC_M,    HOME_N,  HOME_E,  SON_I,   HOME_O,
         KC_Y,    KC_X,    AC_C,    KC_D,    AC_V, XXXXXXX,                                 XXXXXXX, KC_K,    KC_H,    KC_COMM, KC_DOT,  LSFT(KC_MINUS),
-                    LT(_nav,KC_SPC), LT(_num,KC_TAB), winLeft,                        winRight, LT(_sym,KC_ENT),  LSFT_T(KC_BSPC)
+                    LT(_nav,KC_SPC), LT(_num,KC_TAB), LCTL(KC_P6), winLeft,                        winRight, XXXXXXX, LT(_sym,KC_ENT),  LSFT_T(KC_BSPC)
     ),
+
 
 /*
  * Nav Layer: Navigation
@@ -136,11 +139,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
 
-    [_nav] = LAYOUT_split_3x5_3_encoder(
+    [_nav] = LAYOUT_split_3x5_4_encoder(
       XXXXXXX, LCTL(KC_P4), LCTL(KC_P5), LCTL(KC_P6), XXXXXXX,                                     XXXXXXX, KC_INS, KC_DEL, KC_PGUP, XXXXXXX,
       XXXXXXX, KC_P8, KC_P9, KC_VOLU, XXXXXXX,                                                     KC_PSCR, KC_LEFT, KC_UP, KC_RGHT, XXXXXXX,
       XXXXXXX, vscodeFormat, LCTL(KC_C), KC_VOLD, LCTL(KC_V), XXXXXXX,                    XXXXXXX, XXXXXXX, KC_HOME, KC_DOWN, KC_PGDN, KC_END,
-                                        KC_SPC, KC_TAB, XXXXXXX,                          XXXXXXX, KC_ESC, KC_TRNS
+                                    KC_SPC, KC_TAB, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, KC_ESC, KC_TRNS
     ),
 
 /*
@@ -158,11 +161,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
 
-    [_sym] = LAYOUT_split_3x5_3_encoder(
+    [_sym] = LAYOUT_split_3x5_4_encoder(
         RALT(KC_E), RALT(KC_7), RALT(KC_0), LSFT(KC_8), LSFT(KC_9),                                                 RALT(KC_8), RALT(KC_9), LSFT(KC_RBRC), RALT(KC_RBRC), XXXXXXX,
         KC_GRAVE, KC_RBRC, LSFT(KC_0), KC_SLASH, LSFT(KC_4),                                                        LSFT(KC_NUHS), LSFT(KC_2), LSFT(KC_7), LSFT(KC_DOT), LSFT(KC_NUBS),
         LSFT(KC_COMMA), LSFT(KC_5), RALT(KC_NUBS), LSFT(KC_SLASH), RALT(KC_Q), XXXXXXX,                    XXXXXXX, LSFT(KC_1), LSFT(KC_EQUAL), KC_NUHS, LSFT(KC_6), KC_NUBS,
-                                                    KC_ESC, RALT(KC_MINUS), XXXXXXX,                       XXXXXXX, KC_ENT, KC_TRNS
+                                            KC_ESC, RALT(KC_MINUS), XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, KC_ENT, KC_TRNS
     ),
 
 /*
@@ -179,11 +182,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   *                        |      |      |      |      |      |  |      |      |      |      |      |
   *                        `----------------------------------'  `----------------------------------'
   */
-     [_num] = LAYOUT_split_3x5_3_encoder(
+     [_num] = LAYOUT_split_3x5_4_encoder(
        XXXXXXX, KC_7, KC_8, KC_9, XXXXXXX,                                     XXXXXXX, KC_F1, KC_F2,  KC_F3,  KC_F4, 
        KC_0,    KC_4, KC_5, KC_6, XXXXXXX,                                     XXXXXXX, KC_F5, KC_F6,  KC_F7,  KC_F8, 
        XXXXXXX, KC_1, KC_2, KC_3, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, KC_F9, KC_F10, KC_F11, KC_F12,
-                           KC_SPC, KC_TAB, XXXXXXX,                   XXXXXXX, KC_TRNS, KC_TRNS
+                  KC_SPC, KC_TAB, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, KC_TRNS, KC_TRNS
      ),
 
 /*
@@ -200,11 +203,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
-    [_son] = LAYOUT_split_3x5_3_encoder(
+    [_son] = LAYOUT_split_3x5_4_encoder(
        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     XXXXXXX, XXXXXXX, KC_LBRC, XXXXXXX, XXXXXXX,
        KC_QUOTE, XXXXXXX, KC_MINUS, XXXXXXX, XXXXXXX,                                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SCLN,
        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                    KC_SPC, KC_TAB, XXXXXXX,                   XXXXXXX, KC_ESC, KC_TRNS
+                                    KC_SPC, KC_TAB, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, KC_ESC, KC_TRNS
      ),
 
 // /*
@@ -221,7 +224,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //  *                        |      |      |      |      |      |  |      |      |      |      |      |
 //  *                        `----------------------------------'  `----------------------------------'
 //  */
-//     [_LAYERINDEX] = LAYOUT_split_3x5_3_encoder(
+//     [_LAYERINDEX] = LAYOUT_split_3x5_4_encoder(
 //       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
 //       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
 //       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
