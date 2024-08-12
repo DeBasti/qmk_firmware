8e7d8b5e58510e37c7c269ca41e50081d71ec8e3
DeBasti <sebastian.eisenbarth96@gmail.com>
2024-08-12 15:38:53 +0200

    added option to send phrase on first key press

--- keyboards/narfpad/keymaps/default/keymap.c
+++ keyboards/narfpad/keymaps/default/keymap.c
@@ -1,8 +1,24 @@
 #include QMK_KEYBOARD_H
 
+enum custom_keycodes {
+    phrase
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {          // Return true for normal processing of tap keycode
+        case phrase:
+            if (record->event.pressed) {
+                SEND_STRING("[Phrase goes here]");
+                return false;
+            }
+        return true;             // Return true for normal processing of tap keycode
+    }
+    return true;
+}
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-                 KC_F1, KC_F2, KC_F3,
+                 phrase, KC_F2, KC_F3,
                  KC_F5, LSFT(KC_F5), KC_F8,
         KC_MUTE, KC_F9, KC_F10,KC_F11
     )
